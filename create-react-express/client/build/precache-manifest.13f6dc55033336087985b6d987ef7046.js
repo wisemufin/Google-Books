e4754fea20259f7a10adab6f352f29605464cb7b
Matthew Trabold <mct4240@gmail.com>
2019-05-02 16:25:07 -0500

    Initial Commit

---
+++ create-react-express/client/build/precache-manifest.13f6dc55033336087985b6d987ef7046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea90343e000efa69d13b",
+    "url": "/static/js/main.223d47f0.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ea90343e000efa69d13b",
+    "url": "/static/css/main.4a3a8564.chunk.css"
+  },
+  {
+    "revision": "277d108d2ac9766fbd542f994d2edef6",
+    "url": "/index.html"
+  }
+];
