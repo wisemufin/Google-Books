73b9ba03fa30ec0acc979d696f00f2abb79e6092
Matthew Trabold <mct4240@gmail.com>
2019-05-02 17:11:34 -0500

    Second commit

---
+++ create-react-express/models/book.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: { type: String, required: true },
+  description: String,
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
